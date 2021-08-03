0b9bdd7788d1206941edd2eb39ca8afa38a73633
JJong-Min <sambujakkk@naver.com>
2021-08-03 20:59:02 +0900

    update app.py

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import requests
 from pymongo import MongoClient
+
+
 app = Flask(__name__)
 
-
+client = MongoClient('localhost', 27017)
+db = client.week0
 
 @app.route('/')
 def home():
    return render_template('index.html')
 
+@app.route('/login')
+def login():
+   return render_template('login.html')
 
+
+
+@app.route('/join')
+def join():
+   return render_template('join.html')
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
