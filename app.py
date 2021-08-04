9d75e4823d062fbd34857af3f907c0b3fc0b5495
JJong-Min <sambujakkk@naver.com>
2021-08-04 19:18:33 +0900

    update app

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
-
-from logging import DEBUG
-from flask import Flask, render_template, request, jsonify, redirect, url_for, session
-
-
-
-from flask import Flask, request, render_template, jsonify, redirect, url_for, session
+from flask import Flask, json, request, render_template, jsonify, redirect, url_for, session
 import requests, random
-
-
+from flask_bcrypt import Bcrypt
 from pymongo import MongoClient
 import jwt
 import datetime
+from functools import wraps
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'IOJPI241JPI'
+bcrypt = Bcrypt(app)
 client = MongoClient('localhost', 27017)
 db = client.week0
-key = "secret"
 
 
 @app.route('/')
@@ -74,16 +68,22 @@ def login_pro():
    user_pw = request.form['PW_give']
    user_info = db.user_info.find_one({'userID':user_id})
    try:
-      if user_info['userPW'] == user_pw:
-         access_payload = {"id": user_id, "password": user_pw, "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}
-         refresh_payload = {"id": user_id, "password": user_pw, "exp": datetime.datetime.utcnow() + datetime.timedelta(days=30)}
-         return jsonify({"result": "success", 'access_token': jwt.encode(access_payload, key, algorithm="HS256"), 'refresh_token': jwt.encode(refresh_payload, key, algorithm="HS256")})
+      if bcrypt.check_password_hash(user_info['userPW'], user_pw):
+         access_payload = {"id": user_id, "exp": datetime.datetime.utcnow() + datetime.timedelta(seconds=3)}
+         session['logged_in'] = True
+         return jsonify({"result": "success", 'access_token': jwt.encode(access_payload, app.config['SECRET_KEY'], algorithm="HS256")})
       else:
-         return jsonify(result = "fail")
+         return jsonify({"result": "fail"})
    except:
-      return jsonify(result = "fail")
+      return jsonify({"result": "fail"})
 
-@app.route('/join')
+@app.route('/logout')
+def logout():
+    session['logged_in'] = False
+    print(session['logged_in'])
+    return redirect('/')
+
+@app.route('/join', methods=['GET'])
 def join():
    return render_template('join.html')
 
@@ -93,10 +93,11 @@ def join():
 def join_pro():
    user_id = request.form['ID_give']
    user_pw = request.form['PW_give']
+   pw_hash = bcrypt.generate_password_hash(user_pw)
    user_email = request.form['Email_give']
    user_name = request.form['Name_give']
    try:
-      db.user_info.insert_one({'userID':user_id, 'userPW': user_pw, 'userEmail': user_email, 'userName': user_name})
+      db.user_info.insert_one({'userID':user_id, 'userPW': pw_hash, 'userEmail': user_email, 'userName': user_name})
       return jsonify({"result": "success"})
    except:
       return jsonify({'result':'fail'})
@@ -118,6 +119,27 @@ def welcome():
    return render_template('welcome.html')
 
 
+@app.route('/modification', methods=['POST'])
+def modification():
+   user_token = request.form['token_give']
+   if not user_token:
+      return jsonify({'result': 'fail'})
+   try:
+      decoded = jwt.decode(user_token, app.config['SECRET_KEY'], algorithms=["HS256"])
+      existing_user_infos = db.user_info.find_one({'userID':decoded['id']}, {'_id':False})
+      user_name = existing_user_infos['userName']
+      user_id = existing_user_infos['userID']
+      user_email = existing_user_infos['userEmail']
+      return jsonify({'result':'success', 'user_id':user_id, 'user_name': user_name, 'user_email': user_email})
+   except jwt.ExpiredSignatureError:
+      session['logged_in'] = False
+      return jsonify({'result':'fail'})
+
+@app.route('/modication_form')
+def modification_form():
+   return render_template('modification.html')
+      
+      
 if __name__ == '__main__':
    
    app.run('0.0.0.0',port=5000,debug=True)
